2486cf70d102a5265aaa941a2b9f2441ba94c160
surajXtreme25 <surajmaurya983878@gmail.com>
2026-08-29 09:20:43 +0530

    Fix MongoDB Atlas connection

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const reviewsRouter =require("./routes/review.js");
 const userRouter =require("./routes/user.js");
 // Creating a Database
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = process.env.ATLASDB_URL;
 
 main().then(() => {
     console.log("connected to DB");
